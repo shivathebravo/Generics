22e2cd81c6bf104fa330fac665fe56d5dfa75fdc
Shiva <Shiva@SHIVA-PC>
2017-05-01 21:42:13 -0400

    added repo

---
+++ Documents/Visual Studio 2017/Projects/EFCodeFirstMultipleDBContext/CodeFirstConsole/Migrations/201705020109188_createdPost.cs
@@ -0,0 +1,27 @@
+namespace CodeFirstConsole.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class createdPost : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        DatePublished = c.DateTime(nullable: false),
+                        Title = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ Documents/Visual Studio 2017/Projects/EFCodeFirstMultipleDBContext/CodeFirstConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstConsole
+{
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public DateTime DatePublished { get; set; }
+        public string Title { get; set; }   
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BlogDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+
+    public class StudentDBContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
